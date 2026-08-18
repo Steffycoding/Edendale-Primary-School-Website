a4f0d0508572b26c7ce99d3b789b6530528cd1a7
Stephanie Poole <113518813+Steffycoding@users.noreply.github.com>
2026-08-18 13:46:22 +0200

    netlify

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,9 +1,3 @@
-// netlify/functions/api.js
-//
-// Wraps the existing Express app (server.js) so it can run as a single
-// Netlify Function, instead of splitting every route into its own file.
-// netlify.toml redirects all /api/* requests here.
-
 import serverless from 'serverless-http';
 import app from '../../server.js';
 
